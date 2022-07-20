a9c40cc8b473bb51abefc52efa504c7372ae95f7
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-07-20 17:08:20 +0300

    Create Counter

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-class Heading extends React.Component {
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  decrement = () => {
+    if (this.state.count <= 0) {
+      return;
+    }
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
-    const { title, className, children } = this.props;
-    return React.createElement("h1", { title, className }, ...children);
+    const { count } = this.state;
+    return React.createElement(
+      React.Fragment,
+      null,
+      React.createElement("h2", null, count),
+      React.createElement("button", { onClick: this.increment }, "+"),
+      React.createElement("button", { onClick: this.decrement }, "-")
+    );
   }
 }
 
 const container = document.getElementById("root");
-const element = React.createElement(
-  Heading,
-  { title: "Title for h1", className: "heading" },
-  "This is React"
-);
+const element = React.createElement(Counter);
 ReactDOM.render(element, container);
